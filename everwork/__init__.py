2ea4b5791add8dfef480c728c73228f2d6783156
egork <onetells@ya.ru>
2026-02-04 18:27:42 +0500

    draft: Изменение архитектуры

--- everwork/__init__.py
+++ everwork/__init__.py
@@ -1,6 +1,6 @@
+from ._internal.backend import RedisBackend
+from ._internal.broker import RedisBroker
 from ._internal.process.process_manager import ProcessManager
-from .backend import AbstractBackend
-from .broker import AbstractBroker
 from .schemas import (
     Cron,
     Event,
@@ -11,24 +11,23 @@ from .schemas import (
     Trigger,
     WorkerSettings
 )
-from .utils import AbstractCronSchedule, CronSchedule, EventCollector, to_seconds
+from .utils import CronSchedule, EventCollector, to_seconds
 from .workers import AbstractWorker
 
 __all__ = (
     'ProcessManager',
-    'Process',
     'ProcessGroup',
+    'Process',
     'AbstractWorker',
     'WorkerSettings',
     'EventSettings',
-    'Event',
-    'EventCollector',
-    'AbstractBackend',
-    'AbstractBroker',
     'Trigger',
     'Cron',
     'Interval',
+    'Event',
+    'EventCollector',
     'to_seconds',
-    'AbstractCronSchedule',
-    'CronSchedule'
+    'RedisBroker',
+    'RedisBackend',
+    'CronSchedule',
 )
