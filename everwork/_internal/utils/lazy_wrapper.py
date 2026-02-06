8a2a094cf4734b3a16cdb24b3f5464db353fe931
egork <onetells@ya.ru>
2026-02-06 23:45:16 +0500

    Исправление ошибок

--- everwork/_internal/utils/lazy_wrapper.py
+++ everwork/_internal/utils/lazy_wrapper.py
@@ -1,10 +1,10 @@
-from typing import Callable, Type
+from typing import Any, Callable, Type
 
 
-class _SimpleLazyFactory[T, **P]:
+class _LazyFactory[T]:
     __slots__ = ("_cls", "_args", "_kwargs")
 
-    def __init__(self, cls: Type[T], args: tuple, kwargs: dict):
+    def __init__(self, cls: Type[T], args: tuple[Any, ...], kwargs: dict[str, Any]) -> None:
         self._cls = cls
         self._args = args
         self._kwargs = kwargs
@@ -12,10 +12,13 @@ class _SimpleLazyFactory[T, **P]:
     def __call__(self) -> T:
         return self._cls(*self._args, **self._kwargs)
 
+    def __reduce__(self) -> tuple[Callable, tuple]:
+        return type(self), (self._cls, self._args, self._kwargs)
 
-def lazy_init[T, **P](cls: Type[T]) -> Callable[P, _SimpleLazyFactory]:
 
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> _SimpleLazyFactory:
-        return _SimpleLazyFactory(cls, args, kwargs)
+def lazy_init[T, **P](cls: Type[T]) -> Callable[P, _LazyFactory]:
+
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> _LazyFactory:
+        return _LazyFactory(cls, args, kwargs)
 
     return wrapper
