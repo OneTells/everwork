f6cff40b90ca769c2394e4c668ddf3d7d99d1dbb
egork <onetells@ya.ru>
2026-02-06 23:24:04 +0500

    Исправление ошибок

--- everwork/_internal/utils/lazy_wrapper.py
+++ everwork/_internal/utils/lazy_wrapper.py
@@ -1,12 +1,21 @@
 from typing import Callable, Type
 
 
-def lazy_init[T, **P](cls: Type[T]) -> Callable[P, Callable[[], T]]:
+class _SimpleLazyFactory[T, **P]:
+    __slots__ = ("_cls", "_args", "_kwargs")
 
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Callable[[], T]:
-        def create_instance() -> T:
-            return cls(*args, **kwargs)
+    def __init__(self, cls: Type[T], args: tuple, kwargs: dict):
+        self._cls = cls
+        self._args = args
+        self._kwargs = kwargs
 
-        return create_instance
+    def __call__(self) -> T:
+        return self._cls(*self._args, **self._kwargs)
+
+
+def lazy_init[T, **P](cls: Type[T]) -> Callable[P, _SimpleLazyFactory]:
+
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> _SimpleLazyFactory:
+        return _SimpleLazyFactory(cls, args, kwargs)
 
     return wrapper
