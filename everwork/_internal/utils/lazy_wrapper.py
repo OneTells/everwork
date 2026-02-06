66018120b7658b902e119e07645110e5b6929f1e
egork <onetells@ya.ru>
2026-02-06 23:50:30 +0500

    Исправление ошибок

--- everwork/_internal/utils/lazy_wrapper.py
+++ everwork/_internal/utils/lazy_wrapper.py
@@ -1,24 +1,28 @@
+import importlib
 from typing import Any, Callable, Type
 
 
 class _LazyFactory[T]:
-    __slots__ = ("_cls", "_args", "_kwargs")
+    __slots__ = ("_module", "_class_name", "_args", "_kwargs")
 
-    def __init__(self, cls: Type[T], args: tuple[Any, ...], kwargs: dict[str, Any]) -> None:
-        self._cls = cls
+    def __init__(self, module: str, class_name: str, args: tuple[Any, ...], kwargs: dict[str, Any]) -> None:
+        self._module = module
+        self._class_name = class_name
         self._args = args
         self._kwargs = kwargs
 
     def __call__(self) -> T:
-        return self._cls(*self._args, **self._kwargs)
+        module_obj = importlib.import_module(self._module)
+        cls = getattr(module_obj, self._class_name)
+        return cls(*self._args, **self._kwargs)
 
     def __reduce__(self) -> tuple[Callable, tuple]:
-        return type(self), (self._cls, self._args, self._kwargs)
+        return _LazyFactory, (self._module, self._class_name, self._args, self._kwargs)
 
 
-def lazy_init[T, **P](cls: Type[T]) -> Callable[P, _LazyFactory]:
+def lazy_init[T, **P](cls: Type[T]) -> Callable[P, _LazyFactory[T]]:
 
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> _LazyFactory:
-        return _LazyFactory(cls, args, kwargs)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> _LazyFactory[T]:
+        return _LazyFactory(cls.__module__, cls.__name__, args, kwargs)
 
     return wrapper
