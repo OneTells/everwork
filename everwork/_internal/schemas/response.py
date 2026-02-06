924397884a242b276bfec88a2ac3b1ad9ad16e54
egork <onetells@ya.ru>
2026-02-06 18:11:40 +0500

    Исправление ошибок

--- everwork/_internal/schemas/response.py
+++ everwork/_internal/schemas/response.py
@@ -1,11 +1,13 @@
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from everwork._internal.utils.event_storage import AbstractReader
 
 
 class Response(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     status: Literal['ack', 'fail', 'reject', 'retry']
 
 
